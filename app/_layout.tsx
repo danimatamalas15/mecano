1165ce8956f60080bba5b8a0830b70a0e4bceed5
danimatamalas15 <danimatamalas15@gmail.com>
2026-03-09 12:51:25 +0100

    varios

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,36 +1,10 @@
 import { Stack } from "expo-router";
-import { Platform, StyleSheet, View } from "react-native";
 
 export default function RootLayout() {
   return (
-    <View style={styles.container}>
-      <View style={styles.appContainer}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-      </View>
-    </View>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: Platform.OS === 'web' ? '#e2e8f0' : '#ffffff',
-  },
-  appContainer: {
-    flex: 1,
-    width: '100%',
-    maxWidth: Platform.OS === 'web' ? 800 : '100%',
-    alignSelf: 'center',
-    backgroundColor: '#ffffff',
-    ...(Platform.OS === 'web' ? {
-      shadowColor: '#000',
-      shadowOffset: { width: 0, height: 0 },
-      shadowOpacity: 0.1,
-      shadowRadius: 20,
-      elevation: 5,
-    } : {})
-  }
-});
